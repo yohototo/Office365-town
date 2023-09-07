a9a3108d796862517080bf56c6e9400cebf3f23f
yohototo <35169273+yohototo@users.noreply.github.com>
2023-09-07 14:45:44 +0800

    Update O365-connect-aad.ps1
    some improve

--- O365-connect-aad.ps1
+++ O365-connect-aad.ps1
@@ -1,141 +1,112 @@
-param(                         
+param (
     [switch]$noprompt = $false,   ## if -noprompt used then user will not be asked for any input
     [switch]$noupdate = $false,   ## if -noupdate used then module will not be checked for more recent version
     [switch]$debug = $false       ## if -debug create a log file
 )
-<# CIAOPS
-Script provided as is. Use at own risk. No guarantees or warranty provided.
-
-Description - Log into the Azure AD portal
-
-Source - https://github.com/directorcia/Office365/blob/master/O365-connect-aad.ps1
-
-Prerequisites = 1
-1. Ensure azuread module installed or updated
-
-More scripts available by joining http://www.ciaopspatron.com
-
-#>
 
 ## Variables
 $systemmessagecolor = "cyan"
 $processmessagecolor = "green"
 $errormessagecolor = "red"
 $warningmessagecolor = "yellow"
+$logFilePath = "..\o365-connect-aad.txt"
 
-## If you have running scripts that don't have a certificate, run this command once to disable that level of security
-## set-executionpolicy -executionpolicy bypass -scope currentuser -force
+## Function to log messages
+function Log-Message {
+    param (
+        [string]$message,
+        [string]$color = "white"
+    )
+    Write-Host -ForegroundColor $color $message
+    if ($debug) {
+        Add-Content -Path $logFilePath -Value $message
+    }
+}
 
-Clear-Host
 Clear-Host
 if ($debug) {
-    write-host "Script activity logged at ..\o365-connect-aad.txt"
-    start-transcript "..\o365-connect-aad.txt" | Out-Null                                        ## Log file created in parent directory that is overwritten on each run
+    Log-Message "Script activity logged at $logFilePath"
+    Start-Transcript -Path $logFilePath | Out-Null
 }
 
-write-host -foregroundcolor $systemmessagecolor "Azure AD Connection script started`n"
-write-host -ForegroundColor $processmessagecolor "Prompt =",(-not $noprompt)
+Log-Message "Azure AD Connection script started`n" -ForegroundColor $systemmessagecolor
+Log-Message "Prompt = $(-not $noprompt)" -ForegroundColor $processmessagecolor
 
-if (get-module -listavailable -name azuread) {    ## Has the Azure AD PowerShell module been installed?
-    write-host -ForegroundColor $processmessagecolor "Azure AD PowerShell module installed"
+# Check if Azure AD module is installed
+if (Get-Module -ListAvailable -Name AzureAD) {
+    Log-Message "Azure AD PowerShell module installed" -ForegroundColor $processmessagecolor
 }
 else {
-    write-host -ForegroundColor $warningmessagecolor -backgroundcolor $errormessagecolor "[001] - Azure AD PowerShell module not installed`n"
+    Log-Message "[001] - Azure AD PowerShell module not installed`n" -ForegroundColor $warningmessagecolor -BackgroundColor $errormessagecolor
     if (-not $noprompt) {
         do {
-            $response = read-host -Prompt "`nDo you wish to install the Azure AD PowerShell module (Y/N)?"
-        } until (-not [string]::isnullorempty($response))
-        if ($result -eq 'Y' -or $result -eq 'y') {
-            write-host -foregroundcolor $processmessagecolor "Installing Azure AD PowerShell module - Administration escalation required"
-            Start-Process powershell -Verb runAs -ArgumentList "install-Module -Name azuread -Force -confirm:$false" -wait -WindowStyle Hidden
-            write-host -foregroundcolor $processmessagecolor "Azure AD PowerShell module installed"
+            $response = Read-Host -Prompt "`nDo you wish to install the Azure AD PowerShell module (Y/N)?"
+        } until (-not [string]::IsNullOrEmpty($response))
+        if ($response -eq 'Y' -or $response -eq 'y') {
+            Log-Message "Installing Azure AD PowerShell module - Administration escalation required" -ForegroundColor $processmessagecolor
+            Start-Process powershell -Verb RunAs -ArgumentList "Install-Module -Name AzureAD -Force -Confirm:$false" -Wait -WindowStyle Hidden
+            Log-Message "Azure AD PowerShell module installed" -ForegroundColor $processmessagecolor
         }
         else {
-            write-host -foregroundcolor $processmessagecolor "Terminating script"
+            Log-Message "Terminating script" -ForegroundColor $processmessagecolor
             if ($debug) {
-                Stop-Transcript | Out-Null                 ## Terminate transcription
+                Stop-Transcript | Out-Null
             }
-            exit 1                          ## Terminate script
+            exit 1
         }
     }
     else {
-        write-host -foregroundcolor $processmessagecolor "Installing Azure AD PowerShell module - Administration escalation required"
-        Start-Process powershell -Verb runAs -ArgumentList "install-Module -Name azuread -Force -confirm:$false" -wait -WindowStyle Hidden
-        write-host -foregroundcolor $processmessagecolor "Azure AD PowerShell module installed"    
+        Log-Message "Installing Azure AD PowerShell module - Administration escalation required" -ForegroundColor $processmessagecolor
+        Start-Process powershell -Verb RunAs -ArgumentList "Install-Module -Name AzureAD -Force -Confirm:$false" -Wait -WindowStyle Hidden
+        Log-Message "Azure AD PowerShell module installed" -ForegroundColor $processmessagecolor
     }
 }
 
+# Check for module updates
 if (-not $noupdate) {
-    write-host -foregroundcolor $processmessagecolor "Check whether newer version of Azure AD PowerShell module is available"
-    #get version of the module (selects the first if there are more versions installed)
-    $version = (Get-InstalledModule -name azuread) | Sort-Object Version -Descending  | Select-Object Version -First 1
-    #get version of the module in psgallery
-    $psgalleryversion = Find-Module -Name azuread | Sort-Object Version -Descending | Select-Object Version -First 1
-    #convert to string for comparison
-    $stringver = $version | Select-Object @{n='ModuleVersion'; e={$_.Version -as [string]}}
-    $a = $stringver | Select-Object Moduleversion -ExpandProperty Moduleversion
-    #convert to string for comparison
-    $onlinever = $psgalleryversion | Select-Object @{n='OnlineVersion'; e={$_.Version -as [string]}}
-    $b = $onlinever | Select-Object OnlineVersion -ExpandProperty OnlineVersion
-    #version compare
-    if ([version]"$a" -ge [version]"$b") {
-        Write-Host -foregroundcolor $processmessagecolor "Local module $a greater or equal to Gallery module $b"
-        write-host -foregroundcolor $processmessagecolor "No update required"
+    Log-Message "Check whether a newer version of Azure AD PowerShell module is available" -ForegroundColor $processmessagecolor
+    $installedModule = Get-InstalledModule -Name AzureAD | Sort-Object Version -Descending | Select-Object -First 1
+    $onlineModule = Find-Module -Name AzureAD | Sort-Object Version -Descending | Select-Object -First 1
+    if ([Version]$installedModule.Version -ge [Version]$onlineModule.Version) {
+        Log-Message "Local module $($installedModule.Version) is up to date" -ForegroundColor $processmessagecolor
     }
     else {
-        Write-Host -foregroundcolor $warningmessagecolor "Local module $a lower version than Gallery module $b"
-        write-host -foregroundcolor $warningmessagecolor "Update recommended"
+        Log-Message "Local module $($installedModule.Version) is outdated. Online module version is $($onlineModule.Version)" -ForegroundColor $warningmessagecolor
         if (-not $noprompt) {
             do {
-                $response = read-host -Prompt "`nDo you wish to update the Azure AD PowerShell module (Y/N)?"
-            } until (-not [string]::isnullorempty($response))
-            if ($result -eq 'Y' -or $result -eq 'y') {
-                write-host -foregroundcolor $processmessagecolor "Updating Azure AD PowerShell module - Administration escalation required"
-                Start-Process powershell -Verb runAs -ArgumentList "update-Module -Name azuread -Force -confirm:$false" -wait -WindowStyle Hidden
-                write-host -foregroundcolor $processmessagecolor "Azure AD PowerShell module - updated"
+                $response = Read-Host -Prompt "`nDo you wish to update the Azure AD PowerShell module (Y/N)?"
+            } until (-not [string]::IsNullOrEmpty($response))
+            if ($response -eq 'Y' -or $response -eq 'y') {
+                Log-Message "Updating Azure AD PowerShell module - Administration escalation required" -ForegroundColor $processmessagecolor
+                Start-Process powershell -Verb RunAs -ArgumentList "Update-Module -Name AzureAD -Force -Confirm:$false" -Wait -WindowStyle Hidden
+                Log-Message "Azure AD PowerShell module updated" -ForegroundColor $processmessagecolor
             }
             else {
-                write-host -foregroundcolor $processmessagecolor "Azure AD PowerShell module - not updated"
+                Log-Message "Azure AD PowerShell module not updated" -ForegroundColor $processmessagecolor
             }
         }
         else {
-        write-host -foregroundcolor $processmessagecolor "Updating Azure AD PowerShell module - Administration escalation required" 
-        Start-Process powershell -Verb runAs -ArgumentList "update-Module -Name azuread -Force -confirm:$false" -wait -WindowStyle Hidden
-        write-host -foregroundcolor $processmessagecolor "Azure AD PowerShell module - updated"
+            Log-Message "Updating Azure AD PowerShell module - Administration escalation required" -ForegroundColor $processmessagecolor
+            Start-Process powershell -Verb RunAs -ArgumentList "Update-Module -Name AzureAD -Force -Confirm:$false" -Wait -WindowStyle Hidden
+            Log-Message "Azure AD PowerShell module updated" -ForegroundColor $processmessagecolor
         }
     }
 }
 
-write-host -foregroundcolor $processmessagecolor "Azure AD PowerShell module loading"
-Try {
-    Import-Module azuread | Out-Null
+# Load Azure AD module
+Log-Message "Azure AD PowerShell module loading" -ForegroundColor $processmessagecolor
+try {
+    Import-Module AzureAD -ErrorAction Stop | Out-Null
+    Log-Message "Azure AD PowerShell module loaded" -ForegroundColor $processmessagecolor
 }
 catch {
-    Write-Host -ForegroundColor $errormessagecolor "[002] - Unable to load Azure AD PowerShell module`n"
-    Write-Host -ForegroundColor $errormessagecolor $_.Exception.Message
+    Log-Message "[002] - Unable to load Azure AD PowerShell module`n" -ForegroundColor $errormessagecolor
+    Log-Message $_.Exception.Message -ForegroundColor $errormessagecolor
     if ($debug) {
-        Stop-Transcript | Out-Null                ## Terminate transcription
+        Stop-Transcript | Out-Null
     }
     exit 2
 }
-write-host -foregroundcolor $processmessagecolor "Azure AD PowerShell module loaded"
 
-## Connect to Azure AD service
-write-host -foregroundcolor $processmessagecolor "Connecting to Azure AD"
-try {
-    $result = Connect-AzureAD | Out-Null
-}
-catch {
-    Write-Host -ForegroundColor $errormessagecolor "[003] - Unable to connect to Azure AD`n"
-    Write-Host -ForegroundColor $errormessagecolor $_.Exception.Message
-    if ($debug) {
-        Stop-Transcript | Out-Null                 ## Terminate transcription
-    }
-    exit 3 
-}
-
-write-host -foregroundcolor $processmessagecolor "Connected to Azure AD`n"
-write-host -foregroundcolor $systemmessagecolor "Azure AD Connection script finished`n"
-if ($debug) {
-    Stop-Transcript | Out-Null
-}
+# Connect to Azure AD
+Log-Message
